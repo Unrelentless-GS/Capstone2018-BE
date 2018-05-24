6f7f2801477016c324286e3e867a0db9abe1cb9f
Tempest042 <brendan.pert@gmail.com>
2018-05-25 01:48:18 +1000

    Deactived unworking code.

--- js/spotifyJS.js
+++ js/spotifyJS.js
@@ -49,18 +49,18 @@ $(document).ready(function() {
 		jQuery('.results-row').fadeIn();
 	});
 
-	jQuery('.form-control').on('keyup', function(){
-		console.log(jQuery('.form-control').val().length);
-		if(jQuery('.form-control').val().length > 0) {
-			jQuery('.form-control').addClass('form-active');
-			jQuery('.main-frame').addClass('display-hide');
-			jQuery('.results-row').removeClass('display-hide');
-		} else {
-			jQuery('.form-control').removeClass('form-active');
-			jQuery('.main-frame').removeClass('display-hide');
-			jQuery('.results-row').addClass('display-hide');
-		}
-	});
+	//jQuery('.form-control').on('keyup', function(){
+	//	console.log(jQuery('.form-control').val().length);
+	//	if(jQuery('.form-control').val().length > 0) {
+	//		jQuery('.form-control').addClass('form-active');
+	//		jQuery('.main-frame').addClass('display-hide');
+	//		jQuery('.results-row').removeClass('display-hide');
+	//	} else {
+	//		jQuery('.form-control').removeClass('form-active');
+	//		jQuery('.main-frame').removeClass('display-hide');
+	//		jQuery('.results-row').addClass('display-hide');
+	//	}
+	//});
 
 });
 
