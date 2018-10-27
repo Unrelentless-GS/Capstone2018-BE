5d1b9fd783ba0a223b098aadc3639f1251507d76
Tempest042 <brendan.pert@gmail.com>
2018-10-27 21:44:05 +1100

    Remove Debug Line

--- js/jukebox.js
+++ js/jukebox.js
@@ -109,7 +109,6 @@ function UpdateVotes()
 	{
 		if(this.readyState == 4 && this.status == 200) 
 		{
-			console.log(this.responseText);
 			var songData = JSON.parse(this.responseText);
 			//If not null
 			if (songData != null)
